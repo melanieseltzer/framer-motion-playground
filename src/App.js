6d40b5c121545cc75bcb5eb6cd1e2d7909c62a23
Melanie Seltzer <melleh11@gmail.com>
2020-05-09 22:33:44 -0700

    Add basic styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <>
+      <div>
+        <h1>Framer Motion Playground</h1>
+        <p>
+          An exploration into what is possible with the{' '}
+          <a
+            href="https://github.com/framer/motion"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Framer Motion
+          </a>{' '}
+          library.
+        </p>
+
+        <br />
+      </div>
+    </>
+  );
 }
 
 export default App;
