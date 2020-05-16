f58212d267f8f4e20f5b46af11b69ba4507453ab
Melanie Seltzer <melleh11@gmail.com>
2020-05-16 11:15:32 -0700

    Account for duplicate dashes in the slugify util

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,8 @@
 export const slugify = str =>
   str
+    // replace forward slash and spaces with dash
     .replace(/\/|\s+/g, '-')
+    // replace duplicate dashes with single dash
+    .replace(/[\s-]+/g, '-')
     .toLowerCase()
     .trim();
