f1b56ca94f68e14915525d04d38c576ae5350705
Melanie Seltzer <melleh11@gmail.com>
2020-05-16 10:43:21 -0700

    Add animation and style the list

--- src/components/Playgrounds/ShuffleList/ShuffleList.js
+++ src/components/Playgrounds/ShuffleList/ShuffleList.js
@@ -1,12 +1,26 @@
 import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 import { shuffle } from 'lodash';
-// import { motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 import { Section } from '../../Layout';
 
 const FormWrapper = styled.form`
   display: flex;
+`;
+
+const List = styled.ul`
+  padding-left: 0;
+`;
+
+const ListItem = styled(motion.li)`
+  background: linear-gradient(to right, #8e2de2, #4a00e0);
+  list-style: none;
+  margin: 0;
+  margin-bottom: 5px;
+  padding: 5px;
+  color: #fff;
+  font-size: 1.2rem;
 `;
 
 const ShuffleList = () => {
@@ -34,11 +48,13 @@ const ShuffleList = () => {
       </FormWrapper>
 
       {names.length ? (
-        <ol>
+        <List>
           {names.map(name => (
-            <li key={name}>{name}</li>
+            <ListItem key={name} positionTransition>
+              {name}
+            </ListItem>
           ))}
-        </ol>
+        </List>
       ) : (
         <p>No names yet</p>
       )}
